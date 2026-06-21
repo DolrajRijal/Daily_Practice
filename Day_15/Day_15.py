6bd6fbf37afa1ffef9cab3bc9c018f76d3ccf269
Dolraj Rijal <drrijal94@gmail.com>
2026-06-21 16:58:02 +0100

    day_16

--- Day_15/Day_15.py
+++ Day_15/Day_15.py
@@ -6,13 +6,25 @@ def data():
 
     name = input("enter the name:")
     phone_num = int(input("enter the phone number:"))
-    phone_book['name'] = name
-    phone_book['phone_num'] = phone_num
+    phone_book[name] = name
+    phone_book[phone_num] = phone_num
 
 
 def view_data():
-    for name, phone_num in phone_book:
+    for name, phone_num in phone_book.items():
         print(name, phone_num)
 
-print("please select:")
-choice = int(input('1. '))
+while True:
+
+    print("please select:")
+    print('1. add data \n 2.view data ')
+
+
+    choice = int(input("Please enter your choice"))
+
+    if choice == 1:
+        data()
+        print('Data added successfully')
+
+    if choice == 2:
+        view_data()
