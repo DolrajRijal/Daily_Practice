3ec39cfea9087a85ec500081121578b390a10ee1
Dolraj Rijal <drrijal94@gmail.com>
2026-07-08 23:31:23 +0100

    polymorph

---
+++ Day__/day29.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        print("Animal makes a sound")
+
+
+class Dog(Animal):
+    def speak(self):
+        print("Dog barks")
+
+
+class Cat(Animal):
+    def speak(self):
+        print("Cat meows")
+
+
+# Polymorphism
+animals = [Dog(), Cat(), Animal()]
+
+for animal in animals:
+    animal.speak()
